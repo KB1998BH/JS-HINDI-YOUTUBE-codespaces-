7992742f0bdeca804f581af02a8cf0eacfcc6acd
Krishnendu Bhunia <107338444+KB1998BH@users.noreply.github.com>
2023-10-22 06:38:49 +0000

    datatypes_js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict" // treat all js all newer version
+//alert(3 + 3) // we are using node js , not browser
+
+//code readibilty priority
+console.log(3+3) //code readibilty should be high
+
+//Documentation --- tc39 and MDN;
+let name = "hitesh";
+let age = 18;
+let isLoggedIn = false;
+
+//number => to power 53;
+//bigint;
+//string = ""
+//boolean => true/false 
+//null => standalone value;
+//undefined => 
+//symbol => unique;
+
+
+//object
+console.log(typeof null)
+console.log(typeof undefined);
