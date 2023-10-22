6a45c84077134791df8be24529f3fb93de6f8055
Krishnendu Bhunia <107338444+KB1998BH@users.noreply.github.com>
2023-10-22 06:38:25 +0000

    variable_js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountEmail = "Krissh@1998.com";
+var accountPassword = "12345"
+accountcity = "jaipur"
+let accountstate;
+
+// accountId = 2 //--not allowed;
+
+accountEmail = "hc@gmail.com"
+accountPassword = "2121"
+accountcity = "bangaluru"
+
+/* 
+prefer not to use var 
+because the use of block scope and function scope
+*/
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountcity, accountstate])
