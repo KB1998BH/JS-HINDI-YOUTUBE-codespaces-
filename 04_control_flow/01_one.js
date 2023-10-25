320ebe7539e4a5a3f79bd505c726abf49d57c45d
Krishnendu Bhunia <107338444+KB1998BH@users.noreply.github.com>
2023-10-25 06:03:22 +0000

    one.js

---
+++ 04_control_flow/01_one.js
@@ -0,0 +1,26 @@
+if(2 == "2"){
+    console.log("executed");
+}
+
+
+// const score = 200
+// if(score > 100){
+//     var power = "fly"
+//     console.log(`user power: ${power}`);
+// }
+// console.log(`user power: ${power}`);
+
+
+///-------short hand notation 
+const balance = 1000;
+if(balance > 500) console.log("test")
+
+
+//--- nulliesh coaliesching Operator(??) null/ undefined
+let val2;
+//val1 = 5 ?? 10
+// val2 = null ?? 10
+
+//val2 = null ?? 10 ?? 30 //--- first value assign
+
+console.log(val2);
