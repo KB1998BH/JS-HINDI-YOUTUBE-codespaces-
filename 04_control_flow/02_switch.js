c7f5d206dd6b11e8bd7277dae22120d2892bdf0b
Krishnendu Bhunia <107338444+KB1998BH@users.noreply.github.com>
2023-10-25 06:03:38 +0000

    switch.js

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,27 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 8;
+switch (month) {
+    
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("january");
+        break;
+    case 3:
+        console.log("january");
+        break;
+    case 4:
+        console.log("january");
+        break;
+    default:
+        break;
+}
